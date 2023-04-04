db19521fbed39e1ee8eb0b6e64858e4edd3158c3
Ludovic Druette <ludovicdruette@protonmail.com>
2023-04-04 21:31:50 +0200

    Add documentation for freeSurface detection

--- src/freeSurface.py
+++ src/freeSurface.py
@@ -12,6 +12,9 @@ from .spyh import Fw
 
 
 def minEigenvalueR(volPart, er, rNorm, dwdr):
+    """
+    Return the minimum eigenvalue of the inverse of the renormalization matrix
+    """
     nPart = len(rNorm)
     invR = np.zeros((2, 2))
     for j in range(nPart):
@@ -21,25 +24,32 @@ def minEigenvalueR(volPart, er, rNorm, dwdr):
 
 
 def freeSurfaceDetection(
-    partBOUND, partSPID, partPos, partRho, listNeibSpace, aW, h, m
+    partFLUID, partSPID, partPos, partRho, listNeibSpace, aW, h, m
 ):
     """
     Detected isolated particles and free surface using renormalization matrix,
     based on results in Doring (2005)
     input :
-        -
+        - partFLUID : True if a FLUID particle
+        - partSPID : particle space ID
+        - partPOS : particle position
+        - partRho : particle density
+        - listNeibSpace : list of the particle influencing a particle in a space spId
+        - aW : contant of kernel
+        - h : smoothing length
+        - m : particle mass
     output :
         - partLambda : table of fluid particules lambda
         - isolatedPart : table of ID of isolated particles (lambda <= 0.20)
         - freeSurface : table of ID of particles belonging to free surface
                         (0.20 < lambda < 0.75)
     """
-    nPart = len(partBOUND)
-    partLambda = -1 * np.ones_like(partBOUND)
+    nPart = len(partFLUID)
+    partLambda = -1 * np.ones_like(partFLUID)
     isolatedPart = []
     freeSurface = []
     for i in range(nPart):
-        if not partBOUND[i]:
+        if partFLUID[i]:
             spid_i = int(partSPID[i])
             # list neib
             listnb = listNeibSpace[spid_i, :]
